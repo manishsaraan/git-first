9fdcafd0f9e93ef6d86b81140365f2b196c5cbec
manishsaraan <manish021js@gmail.com>
2019-05-18 21:31:35 +0530

    inital structure changed to react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Select from "react-select";
+import languages from "./assets/lang.json";
+import "./App.css";
+
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" }
+];
 
 function App() {
+  const [selectedOption, updateSelectedOption] = useState(null);
+
+  const handleChange = selectedOption => {
+    updateSelectedOption(selectedOption);
+    console.log(`Option selected:`, selectedOption);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="hero-text jumbotron">
+        Search beginner friendly projects on Github
+      </div>
+      <div className="selector-container">
+        <div className="row">
+          <div className="col-6">
+            <span>max stars:</span>
+            <div id="slider" />
+          </div>
+          <div className="col-3">
+            <span>language:</span>
+            <Select
+              value={selectedOption}
+              onChange={handleChange}
+              options={languages}
+            />
+          </div>
+          <div className="col-3">
+            <button type="button" id="search">
+              search
+            </button>
+          </div>
+        </div>
+      </div>
+      <div id="output" className="row" />
     </div>
   );
 }

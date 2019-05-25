88b9683122c4e064b84ab0a1e0593bf2fc968d26
manishsaraan <manish021js@gmail.com>
2019-05-25 21:51:40 +0530

    Contorls component created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { useState } from "react";
-import Select from "react-select";
-import InputRange from "react-input-range";
-import languages from "./assets/lang.json";
 import fetchRepos from "./lib";
 import Repo from "./components/Repo";
 import Loader from "./components/Loader/Loader";
+import Controls from "./components/Controls/Controls";
 import "./App.css";
 
 const App = () => {
-  const [selectedLanguage, updateSelectedLanguage] = useState(null);
-  const [stars, updateStars] = useState(50);
   const [repos, updateRepos] = useState([]);
 
-  const handleChange = selectedLanguage => {
-    updateSelectedLanguage(selectedLanguage);
-  };
-
-  const exploreProjects = () => {
+  const exploreProjects = (stars, selectedLanguage) => {
     console.log(stars, selectedLanguage);
     fetchRepos({ language: selectedLanguage.value, stars }, repos => {
       console.log(repos);
@@ -29,37 +21,7 @@ const App = () => {
       <div className="hero-text jumbotron">
         Search beginner friendly projects on Github
       </div>
-      <div className="selector-container">
-        <div className="row">
-          <div className="col-6 offset-md-1">
-            <span>max stars:</span>
-            <InputRange
-              maxValue={1000}
-              minValue={10}
-              value={stars}
-              onChange={value => updateStars(value)}
-            />
-          </div>
-          <div className="col-3">
-            <span>language:</span>
-            <Select
-              value={selectedLanguage}
-              onChange={handleChange}
-              options={languages}
-            />
-          </div>
-          <div className="col-1">
-            <button
-              className="btn btn-secondary btn-lg explore-button"
-              type="button"
-              id="search"
-              onClick={exploreProjects}
-            >
-              Explore
-            </button>
-          </div>
-        </div>
-      </div>
+      <Controls exploreProjects={exploreProjects} />
       <div id="output" className="row">
         <Loader />
         {repos.map(repo => (
---
+++ src/components/Controls/Controls.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Select from "react-select";
+import InputRange from "react-input-range";
+import languages from "../../assets/lang.json";
+
+const Controls = props => {
+  const [selectedLanguage, updateSelectedLanguage] = useState(null);
+  const [stars, updateStars] = useState(50);
+
+  const handleChange = selectedLanguage => {
+    updateSelectedLanguage(selectedLanguage);
+  };
+
+  const handleSubmit = () => props.exploreProjects(stars, selectedLanguage);
+
+  return (
+    <div className="selector-container">
+      <div className="row">
+        <div className="col-6 offset-md-1">
+          <span>max stars:</span>
+          <InputRange
+            maxValue={1000}
+            minValue={10}
+            value={stars}
+            onChange={value => updateStars(value)}
+          />
+        </div>
+        <div className="col-3">
+          <span>language:</span>
+          <Select
+            value={selectedLanguage}
+            onChange={handleChange}
+            options={languages}
+          />
+        </div>
+        <div className="col-1">
+          <button
+            className="btn btn-secondary btn-lg explore-button"
+            type="button"
+            id="search"
+            onClick={handleSubmit}
+          >
+            Explore
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Controls;
